68c3276083d829211b3645b352662193e34c0616
corilla007 <100362733+corilla007@users.noreply.github.com>
2022-04-21 18:25:32 +0200

    update

--- funktionen.py
+++ funktionen.py
@@ -1,14 +1,92 @@
+import json
 
-def erfassen_speichern_facher (name_subject_session_antwort,
-                       name_topic_session_antwort,
-                       name_time_session_antwort,
-                       name_time_session_antwort,
-                       control_session_antwort,):
+# Funktion zum Öffnen der Datenbank "faecher".
+def open_faecher():
+    try:
+        with open("datenbank_faecher.json", "r", encoding="utf-8") as datenbank_faecher:
+            # Inhalt der Datenbank wird als Dictonary gespeichert.
+            faecher = json.load(datenbank_faecher)
+    except:
+        faecher = {}
+
+    return faecher
+
+# Funktion zum Öffnen der Datenbank "datenbank_lernsessions".
+def open_lernsessions():
+    try:
+        with open("datenbank_lernsessions.json", "r", encoding="utf-8") as datenbank_lernsessions:
+            # Inhalt der Datenbank wird als Dictonary gespeichert.
+            lernsessions = json.load(datenbank_lernsessions)
+    except:
+        lernsessions = {}
+
+    return lernsessions
+
+# Funktion zum Öffnen der Datenbank "datenbank_vorschlaege_lernsessions".
+def open_vorschlaege():
+    try:
+        with open("datenbank_vorschlaege_lernsessions.json", "r", encoding="utf-8") as datenbank_vorschlaege_lernsessions:
+            # Inhalt der Datenbank wird als Dictonary gespeichert.
+            vorschlaege = json.load(datenbank_vorschlaege_lernsessions)
+    except:
+        vorschlaege = {}
+
+    return vorschlaege
+
+# Funktion zum Öffnen der Datenbank "datenbank_vorschlaege_lernsessions".
+def open_vorschlaege():
+    try:
+        with open("datenbank_vorschlaege_lernsessions.json", "r", encoding="utf-8") as datenbank_vorschlaege_lernsessions:
+            # Inhalt der Datenbank wird als Dictonary gespeichert.
+            vorschlaege = json.load(datenbank_vorschlaege_lernsessions)
+    except:
+        vorschlaege = {}
+
+    return vorschlaege
 
 
+def erfassen_speichern_faecher (name_faecher_erfassen_speichern,
+                               name_study_antwort,
+                               name_semester_antwort,
+                               name_subjects_antwort):
 
-def erfassen_speichern_lernsession (name_subject_session_antwort,
-                       name_topic_session_antwort,
-                       name_time_session_antwort,
-                       name_time_session_antwort,
-                       control_session_antwort,):
+    # Funktion open_faecher wird ausgeführt, gespeicherte Fächer werden geöffnet.
+# faecher = open_faecher()
+#     # Neuer Dictionary wird mit den eingetragenen Daten befüllt.
+#     faecher = {
+#         name_faecher_erfassen_speichern: {
+#             "Studium": name_study_antwort
+#             "Semester": name_semester_antwort
+#             "Fächer": name_subjects_antwort
+#         }
+#     }
+
+def erfassen_speichern_lernsession (name_lernsessions_erfassen_speichern,
+                                    name_subject_session_antwort,
+                                    name_topic_session_antwort,
+                                    name_time_session_antwort,
+                                    control_session_antwort):
+
+    # Funktion open_lernsessions wird ausgeführt, gespeicherte Fächer werden geöffnet.
+    lernsessions = open_lernsessions()
+
+    # Neuer Dictionary wird mit den eingetragenen Daten befüllt.
+    lernsessions = {
+        name_lernsessions_erfassen_speichern: {
+            "Fach": name_subject_session_antwort,
+            "Thema": name_topic_session_antwort,
+            "Lernzeit": name_time_session_antwort,
+            "Beherrschungsgrad": control_session_antwort
+        }
+    }
+# Gespeicherte Lernsession werden mit neuer Lernsession ergänzt.
+lernsessions.update(lernsession)
+
+# Der Neue Dictonary wird in der Json-Datei gespeichert.
+with open('datenbank.lernsessions.json', 'w') as datenbank_lernsessions:
+    json.dump(lernsessions, datenbank_lernsessions)
+
+return name_subject_session_antwort, \
+       name_topic_session_antwort, \
+       name_time_session_antwort, \
+       control_session_antwort
--- main.py
+++ main.py
@@ -1,53 +1,54 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
 from flask import url_for
 from flask import request
 
-app = Flask(__name__)
+from funktionen import erfassen_speichern_faecher
 
-@app.route('/')
+app = Flask('Lernsessionmaster')
+
+@app.route("/")
 def home():
-    name = "Lernsessionmaster"
-        cards = [
-            {"titel": "Fächer erfassen", "inhalt": "In der Rubrik "Fächer erfassen" kannst du deine aktuellen Fächer in eine Datenbank abspeichern"},
-            {"titel": "Lernsession erfassen", "inhalt": "In der Rubrik "Lernsession erfassen" kannst du deine erledigten Lernsession dokumentieren"},
-            {"titel": "Vorschläge", "inhalt": "In der Rubrik Vorschläge kannst du verschiedene Kriterien eingeben, erhältst einen Vorschlag für eine Lernsession, welche dann als offener To-Do Punkt unter "Übersicht" aufgelistet wird."},
-            {"titel": "Übersicht", "inhalt": "Auf der Übersicht findest du die offenen Vorschläge für deine Lernsessions, welche du noch abarbeiten musst.
-                    Ausserdem befindet sich auf der Seite eine Analyse zu deinen erledigten Lernsessions."}
-        ]
     # Rendern des index.html Templates, für das Anzeigen der index.html Page.
-        return render_template("start.html", name=name, cards=cards)
+    return render_template("index.html")
 
 @app.route("/faecher-erfassen", methods= ["get", "post"])
 def study_erfassen():
     if request.method.lower() == "post":
-        name_study_antwort = request.form['study']
-        name_semester_antwort = request.form['semester']
-        name_subjects_antwort = request.form['subjects']
+        # Die Antworten aus dem Formular faecher-erfassen.html werden in Variablen gespeichert.
+        name_study_antwort = request.form["study"]
+        name_semester_antwort = request.form["semester"]
+        name_subjects_antwort = request.form["subjects"]
+        # Die Funktion erfassen_speichern_faecher aus funktionen.py wird mit den Variablen ausgeführt.
+        erfassen_speichern_faecher(name_faecher_erfassen_speichern,
+                                   name_study_antwort,
+                                   name_semester_antwort,
+                                   name_subjects_antwort)
+        return render_template("faecher_erfassen.html", name=name_faecher_erfassen_speichern)
+    return redirect(url_for("lernsession_erfassen.html")
 
-        erfassen_speichern_faecher (name_study_antwort,
-                           name_semester_antwort,
-                           name_time_session_antwort,
-                           name_subjects_antwort)
 
-    return render_template('facher_erfassen.html')
-
-
-@app.route("/lernsession-erfassen", methods=["get", "post"])
+@app.route("/lernsession-erfassen", methods= ["get", "post"])
 def lernsession_erfassen():
     if request.method.lower() == "post":
-        name_subject_session_antwort = request.form['subject_session']
-        name_topic_session_antwort = request.form['topic_session']
-        name_time_session_antwort = request.form['time_session']
+        # Die Antworten aus dem Formular faecher-erfassen.html werden in Variablen gespeichert.
+        name_subject_session_antwort = request.form["subject_session"]
+        name_topic_session_antwort = request.form["topic_session"]
+        name_time_session_antwort = request.form["time_session"]
+        # Antwort wird in Integer umgewandelt - damit kann später gerechnet werden.
         name_time_session_antwort = int(name_time_session_antwort)
-        control_session_antwort = request.form['control_session']
+        control_session_antwort = request.form["control_session"]
 
-        erfassen_speichern_lernsession (name_subject_session_antwort,
-                           name_topic_session_antwort,
-                           name_time_session_antwort,
-                           name_time_session_antwort,
-                           control_session_antwort)
-    return render_template('lernsession_erfassen.html')
+        # Die Funktion erfassen_speichern_lernsession aus funktionen.py wird mit den Variablen durchgeführt
+        erfassen_speichern_lernsession(name_lernsessions_erfassen_speichern,
+                                       name_subject_session_antwort,
+                                       name_topic_session_antwort,
+                                       name_time_session_antwort,
+                                       control_session_antwort)
+
+    return render_template("lernsession_erfassen.html", name=name_lernsessions_erfassen_speichern)
+
 
 
 
@@ -56,10 +57,11 @@ def vorschlaege_ausgabe():
     if request.method.lower() == "post":
 
 
-@app.route("/uebersicht")
-def uebersicht():
-    return render_template('uebersicht.html',
-                           lernsessions_gespeichert=lernsessions_gespeichert_oeffnen())
+#@app.route("/uebersicht")
+#def uebersicht():
+    #
+#   return render_template('uebersicht.html',
+#                          lernsessions_gespeichert=lernsessions_gespeichert_oeffnen())
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
